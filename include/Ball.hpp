9cbcbecd96d51e5bc9447d09db1573356569bbf8
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-10-01 20:39:01 +0200

    the ball is moving + creation of some methods to handle the ball's physics

--- include/Ball.hpp
+++ include/Ball.hpp
@@ -3,6 +3,7 @@
 
 #include <Entity.hpp>
 #include <SFML/Graphics.hpp>
+#include <cassert>
 
 class Ball : public Entity {
 
@@ -12,13 +13,18 @@ class Ball : public Entity {
 public:
   
 private:
+  sf::Vector2f mDirectionVector;
+  float mSpeed;
   
   //METHODS
   
 public: 
   Ball(sf::Vector2f position, sf::Vector2f sides = sf::Vector2f(20.f, 20.f));
   virtual void update(sf::Time dt);
-  
+  void changeDirection(sf::Vector2f directionVector);
+  void accelerate(float acceleration);
+  void decelerate(float deceleration);
+    
 private:
   
 };
--- src/Ball.cpp
+++ src/Ball.cpp
@@ -2,9 +2,27 @@
 
 Ball::Ball(sf::Vector2f position, sf::Vector2f sides)
   : Entity(position, sides)
+  , mDirectionVector(1.f, 1.f)
+  , mSpeed(150)
 {
 }
 
 void Ball::update(sf::Time dt) {
-  //to be implemented 
+  sf::Vector2f moveVector(mDirectionVector.x * mSpeed, mDirectionVector.y * mSpeed);
+  move(moveVector, dt);
+}
+
+void Ball::changeDirection(sf::Vector2f directionVector) {
+  assert(directionVector.x >= -1.f && directionVector.x <= 1.f && directionVector.y >= -1.f && directionVector.y <= 1.f);
+  mDirectionVector = directionVector;
+}
+
+void Ball::accelerate(float acceleration) {
+  assert(acceleration >= 0.f);
+  mSpeed += acceleration;
+}
+
+void Ball::decelerate(float deceleration) {
+  assert(deceleration <= 0.f);
+  mSpeed -= deceleration;
 }
